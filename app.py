f467ba1530fd01be2928252eaa63b125c0e5de27
urich <bapsiooo@gmail.com>
2026-09-18 22:27:39 +0200

    mise à jour https.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit.components.v1 as components
 
 st.title("Mon dashboard Power BI du crédit breton")
 
-powerbi_url = "https://app.powerbi.com/links/Nf5ZtJJ1Mx?ctid=9a50b308-0f88-47aa-b736-35f811873955&pbi_source=linkShare"
+powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=6bb0bcd0-418c-4024-9f37-7cf6cafa830c&autoAuth=true&ctid=9a50b308-0f88-47aa-b736-35f811873955"
 
 components.iframe(
     powerbi_url,
