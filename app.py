cc816ee0837177dfe398b7c2cf1f1e59965dce50
urich <bapsiooo@gmail.com>
2026-09-18 23:50:59 +0200

    Mise a jour du layout Streamlit

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ st.markdown(
 )
 st.title("Mon dashboard du crédit breton")
 
-powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=6bb0bcd0-418c-4024-9f37-7cf6cafa830c&autoAuth=true&ctid=9a50b308-0f88-47aa-b736-35f811873955&actionBarEnabled=true&reportCopilotInEmbed=true"
+powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=6bb0bcd0-418c-4024-9f37-7cf6cafa830c&autoAuth=true&ctid=9a50b308-0f88-47aa-b736-35f811873955"
 
 
 
