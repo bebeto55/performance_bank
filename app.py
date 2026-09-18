cac7af8863a6b7521bd966fc2ddf7fde10bc6914
urich <bapsiooo@gmail.com>
2026-09-18 23:26:33 +0200

    Mise a jour du layout Streamlit

--- app.py
+++ app.py
@@ -1,12 +1,41 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.title("Mon dashboard Power BI du crédit breton")
+import streamlit as st
+import streamlit.components.v1 as components
 
-powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=6bb0bcd0-418c-4024-9f37-7cf6cafa830c&autoAuth=true&ctid=9a50b308-0f88-47aa-b736-35f811873955"
+# 1. Configuration en mode wide
+st.set_page_config(layout="wide")
+
+# 2. Supression des marges latérales de Streamlit via du CSS
+st.markdown(
+    """
+    <style>
+        /* Supprime les marges à gauche et à droite */
+        .appview-container .main .block-container {
+            padding-top: 1rem !important;
+            padding-bottom: 1rem !important;
+            padding-left: 0rem !important;
+            padding-right: 0rem !important;
+            max-width: 100% !important;
+        }
+        /* Force l'élément iframe à prendre tout l'espace disponible */
+        iframe {
+            width: 100% !important;
+        }
+    </style>
+""",
+    unsafe_allow_html=True,
+)
+st.title("Mon dashboard du crédit breton")
+
+powerbi_url = "https://app.powerbi.com/reportEmbed?reportId=6bb0bcd0-418c-4024-9f37-7cf6cafa830c&autoAuth=true&ctid=9a50b308-0f88-47aa-b736-35f811873955&actionBarEnabled=true&reportCopilotInEmbed=true"
+
+
 
 components.iframe(
     powerbi_url,
-    height=700,
+    height=600,
+    width=None,
     scrolling=True
 )
